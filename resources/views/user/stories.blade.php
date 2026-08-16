57382155b1ee3f580251e06589d1124d90821b58
Muniba Khan <munibajavaidkhan@gmail.com>
2026-08-17 01:46:44 +0500

    Fix all pages CSS: replace asset() with root-relative /assets/ paths for CSS files

--- resources/views/user/stories.blade.php
+++ resources/views/user/stories.blade.php
@@ -7,7 +7,7 @@
 
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,500;0,600;0,700;1,600&family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
-<link rel="stylesheet" href="{{ asset('assets/css/stories.css') }}">
+<link rel="stylesheet" href="/assets/css/stories.css">
 </head>
 <body>
 
